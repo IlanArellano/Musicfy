707e09670c499214132a5fc373da93d8e1cf58ef
IlanArellano22 <110840319+IlanArellano22@users.noreply.github.com>
2023-07-22 17:58:02 -0600

    Cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import "./App.css";
-import { ViewManager } from "@ihaz/react-ui-utils";
+import { viewManager } from "./ui/view";
 
 function App() {
   return (
     <main>
-      <ViewManager.Component />
+      <viewManager.Component />
       <RouterProvider router={router} />
     </main>
   );
---
+++ src/ui/view.ts
@@ -0,0 +1,3 @@
+import { ViewManager } from "@ihaz/react-ui-utils";
+
+export const viewManager = ViewManager.createViewManager();
